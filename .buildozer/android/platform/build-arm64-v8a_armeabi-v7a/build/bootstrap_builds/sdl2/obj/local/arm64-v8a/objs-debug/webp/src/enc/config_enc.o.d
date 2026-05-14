2e761edd0d343fa0ef1d260eaec8f34e75326a01
Carlos-Hudson <sos.carlos.hudson@gmail.com>
2026-05-14 05:23:52 -0300

    Limpando cache de submódulos antigos

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,6 +0,0 @@
-/home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-/home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-/home/carlos/Projetos/Glicemia/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
